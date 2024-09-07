9f5ad83ec85871c2fa4137b730c9ea5cfff1c5ea
Carmel Amarilio <carmelamarilio@gmail.com>
2024-09-07 22:45:22 +0300

    F/ fix gitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "/Tedooo-Feed-frontend",
   server: {
     port: 3000
   }
